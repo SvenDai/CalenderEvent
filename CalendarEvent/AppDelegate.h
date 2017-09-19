46ebd29feeeda52fa733f216ca898c304e755f32
fdai <svendai@fdaideMacBook-Pro.local>
2017-09-19 23:56:13 +0800

    Initial Commit

---
+++ CalendarEvent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalendarEvent
+//
+//  Created by fdai on 2017/9/19.
+//  Copyright © 2017年 fdai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
